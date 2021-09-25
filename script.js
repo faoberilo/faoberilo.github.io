e1f6129751ff9a7b8ec7ea18345b8e2efb82bff3
faoberilo <89050385+faoberilo@users.noreply.github.com>
2021-09-25 16:34:24 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,33 +2,33 @@ const botao = document.querySelector("#botao");
 const nome = document.querySelector("#nome");
 const situacao = document.querySelector("#situacao");
 const imagem = document.querySelector("#goku");
-var i=1;
+
 botao.addEventListener("click", function (){
-    if (i===0){
+    if (nome.innerText ==="Nome: GokuGT"){
         nome.innerText ="Nome: Son Goku";
         situacao.innerText = "Situação: Ser Humano";
         imagem.src = "./assets/img/goku01.png";
         i++;
     }
-    else if (i===1){
+    else if (nome.innerText ==="Nome: Son Goku"){
         nome.innerText ="Nome: Goku";
         situacao.innerText = "Situação: SSJ1";
         imagem.src = "./assets/img/goku02.png";
         i++;
     }
-    else if (i===2){
+    else if (nome.innerText ==="Nome: Goku"){
         nome.innerText ="Nome: Kakaroto";
         situacao.innerText = "Situação: SSJ2";
         imagem.src = "./assets/img/goku03.png";
         i++;
     }
-    else if (i===3){
+    else if (nome.innerText ==="Nome: Kakaroto"){
         nome.innerText ="Nome: Cabeludo";
         situacao.innerText = "Situação: SSJ3";
         imagem.src = "./assets/img/goku04.png";
         i++;
     }
-    else if (i===4){
+    else if (nome.innerText ==="Nome: Cabeludo"){
         nome.innerText ="Nome: GokuGT";
         situacao.innerText = "Situação: SSJ4";
         imagem.src = "./assets/img/goku05.png";
